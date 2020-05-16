64a1ee7b7dd2f88a475c96fb7054eac543a8d0d9
Kishor Naik <kishor.naik011.net@gmail.com>
2020-05-16 21:09:02 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/DistributedCacheDemoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Sol_Demo.Models;
+
+namespace Sol_Demo.Controllers
+{
+    public class DistributedCacheDemoController : Controller
+    {
+        private readonly IDistributedCache distributedCache = null;
+
+        public DistributedCacheDemoController(IDistributedCache distributedCache)
+        {
+            this.distributedCache = distributedCache;
+            CacheM = new CacheModel();
+        }
+
+        public CacheModel CacheM { get; set; }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentDateTimeObject = DateTime.Now.ToString();
+
+           var cacheDateTime=await  this.distributedCache.GetStringAsync("CacheDateTime");
+            if (cacheDateTime == null)
+            {
+                var distributeCacheEntryOption = new DistributedCacheEntryOptions()
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10)
+                };
+                await this.distributedCache.SetStringAsync("CacheDateTime", currentDateTimeObject, distributeCacheEntryOption);
+                cacheDateTime = await this.distributedCache.GetStringAsync("CacheDateTime");
+            }
+
+            CacheM.CurrentDateTime = currentDateTimeObject;
+            CacheM.CacheDateTime = cacheDateTime;
+
+            return View(CacheM);
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Models/CacheModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Models
+{
+    public class CacheModel
+    {
+        public String CurrentDateTime { get; set; }
+
+        public String CacheDateTime { get; set; }
+    }
+}
